dfaf7c5d4a3d91d42f660924433f6044ef53fd42
mr-aarjit <aarjitbaral2067@gmail.com>
2026-04-18 16:13:55 +0545

    fix ALLOWED_HOSTS issue

--- Project/settings.py
+++ Project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bo%eccgrn5g2jdtxm-nvsvl+&7r3b7knthde(&6x+zyexv(hsj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "your-app-name.onrender.com"]
 
